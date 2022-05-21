0bd405db3ca2cb9f6f205764e8d916393e0ff969
OscarGlez01 <60475217+OscarGlez01@users.noreply.github.com>
2022-05-20 17:29:41 -0700

    cambios en form

--- Cita/app/src/main/java/mx/itson/cita/CitaConsultaActivity.java
+++ Cita/app/src/main/java/mx/itson/cita/CitaConsultaActivity.java
@@ -3,12 +3,18 @@ package mx.itson.cita;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class CitaConsultaActivity extends AppCompatActivity {
+public class CitaConsultaActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cita_consulta);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- Cita/app/src/main/java/mx/itson/cita/CitaFormActivity.java
+++ Cita/app/src/main/java/mx/itson/cita/CitaFormActivity.java
@@ -2,13 +2,32 @@ package mx.itson.cita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import mx.itson.cita.entidades.Cita;
 
 public class CitaFormActivity extends AppCompatActivity {
+
+    Button btn_guardar;
+    TextView txtFolio;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cita_form);
+        context = this;
+
+        Intent intentCrear = getIntent();
+        Cita cita = (Cita) intentCrear.getSerializableExtra("cita");
+
+        txtFolio = findViewById(R.id.txtFolio);
+        txtFolio.setText(cita.getFolio());
+
     }
 }
